856e75658b02982c839433f829316aaef49438d2
Anton <anton.skalaban@mail.ru>
2022-11-05 23:48:50 +0300

    feat: add solution task 04

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,14 +7,14 @@ fs.readdir(pathFolder, (err, files) => {
     if(err) throw err
       files.forEach(file => { 
       const obj = path.parse(file)
-        fs.stat(pathFolder+'/'+file, (err,stats) => {
+        fs.stat(pathFolder+'\\'+file, (err,stats) => {
           if(err) throw err
           if(stats.isFile()) {
           console.log(`${obj.name} - ${obj.ext.substring(1)} - ${stats.size}B`)
         }
         })
-       
-      } 
-     )})
+      }
+    )
+})
 
  
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs')
+const path = require('path')
 
+const pathDir = path.join(__dirname, 'files')
+const newPathDir = path.join(__dirname, 'files-copy')
+
+const func = async () => {
+    try {
+      await fs.promises.mkdir(newPathDir, {recursive: true})
+      const dirFiles = await fs.promises.readdir(pathDir)
+      dirFiles.forEach(file => fs.promises.copyFile(pathDir + '\\' + file, newPathDir  + '\\' + file))
+    }
+    catch(err) {
+      console.log(err)
+    }
+}
+
+func()
+
+// fs.mkdir(newPathDir, {recursive: true} , err => {
+//     
+// })
+
+// fs.readdir(pathDir, (err, files) => {
+//      files.forEach((file) => {
+//         fs.copyFile(pathDir + '\\' + file, newPathDir  + '\\' + file, err => {
+//             if(err) throw err
+//         })
+//     })
+// })
